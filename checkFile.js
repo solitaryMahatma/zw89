d452ef7229d41de8d8e960aa79fa70ff9142b21e
xiaobai <36806823+solitaryMahatma@users.noreply.github.com>
2023-05-25 00:18:45 +0800

    Create checkFile.js

---
+++ checkFile.js
@@ -0,0 +1,18 @@
+const checkFile = (file:any, types = '.exe,.pdf', max = 10*1024*1024) =>{
+    // max : 10MB
+    const fileName = file.name
+    const fileSize = file.size
+    const [fileType] = ((fileName||'').split('.').map((v:string)=>v.toLowerCase())).splice(-1,1)
+    const fileTypes = types.split(',').map((v:string)=>v.toLowerCase())
+    if (fileTypes.includes(fileType)) {
+      if (fileSize>max) {
+        const aS= max/1024/1024 + 'MB'
+        return Promise.reject({msgCn:`文件(${fileName})太大，超过${aS}`, msg: `The file(${fileName}) is too large, exceeding ${aS}`, type: 1})
+      } else {
+        return Promise.resolve({msgCn:`文件(${fileName})可以上传`,msg:`File(${fileName}) can be uploaded`, type: 2})
+      }
+      
+    } else {
+      return Promise.reject({msgCn:`文件(${fileName})格式不对,支持文件格式有:${types}`, msg:`The file(${fileName}) format is incorrect. Supported file formats are:${types}`, type: 0})
+    }
+  }

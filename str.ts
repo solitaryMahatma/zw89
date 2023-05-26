e494099fabd336979ca9efd1906954f44a172e0e
xiaobai <36806823+solitaryMahatma@users.noreply.github.com>
2023-05-26 22:50:07 +0800

    Update str.ts

--- str.ts
+++ str.ts
@@ -1,6 +1,6 @@
 const wbReplace = (str:string='', regKey:string = '括号',replaceStr:string='' ) => {
     const regObj:any ={
-      '括号': /\((.+?)\)/g
+      '括号': /\((.*?)\)/g   // 112212(1112),7888(2223),8999() <=>112212,7888,8999
     }
     return str.replace(regObj[regKey], replaceStr)
 }
